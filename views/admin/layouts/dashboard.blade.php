3c127fa9dac410888930891fdcae5c292fc14b96
Tihomir Tonov <tishoj@gmail.com>
2017-09-10 19:05:15 +0300

    Fix some routes used in the dashboard view

--- views/admin/layouts/dashboard.blade.php
+++ views/admin/layouts/dashboard.blade.php
@@ -22,7 +22,11 @@
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="{{route('profile.edit', Auth::user()->hash)}}"><i class="fa fa-user fa-fw"></i> {{ trans("blogify::navigation.profile") }} </a>
+                        <li>
+                            <a href="{{ route('admin.profile.edit', Auth::id()) }}">
+                                <i class="fa fa-user fa-fw"></i>
+                                {{ trans('blogify::navigation.profile') }}
+                            </a>
                         </li>
                         <li class="divider"></li>
                         <li><a href="{!! route('admin.logout') !!}"><i class="fa fa-sign-out fa-fw"></i> {{ trans("blogify::navigation.logout") }} </a>
@@ -81,10 +85,10 @@
                                 <a href="#"><i class="fa fa-tags fa-fw"></i>{{ trans("blogify::navigation.tags.title") }}<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
                                     <li>
-                                        <a href="{{ route ('tags.create') }}"><span class="fa fa-plus fa-fw"></span> {{ trans("blogify::navigation.tags.new") }}</a>
+                                        <a href="{{ route ('admin.tags.create') }}"><span class="fa fa-plus fa-fw"></span> {{ trans("blogify::navigation.tags.new") }}</a>
                                     </li>
                                     <li>
-                                        <a href="{{ route ('tags.index' ) }}"><span class="fa fa-th-list fa-fw"></span> {{ trans("blogify::navigation.tags.overview") }}</a>
+                                        <a href="{{ route ('admin.tags.index' ) }}"><span class="fa fa-th-list fa-fw"></span> {{ trans("blogify::navigation.tags.overview") }}</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
