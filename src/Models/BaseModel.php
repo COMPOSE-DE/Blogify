bbd8ab9863dc598c9084004a8c377ea5a8518ad3
Tihomir Tonov <tishoj@gmail.com>
2017-09-10 18:49:27 +0300

    Unguard the base model

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-
+    protected $guarded = [];
     /*
     |--------------------------------------------------------------------------
     | Scopes
