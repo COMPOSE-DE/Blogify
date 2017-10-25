abc1759d6092e84fef266b4e2264d257a9b4c3b4
Leif Matthiessen <lm@compose.de>
2017-10-25 16:28:36 +0200

    Change wording of review mail

--- views/mails/notifyReviewer.blade.php
+++ views/mails/notifyReviewer.blade.php
@@ -1,14 +1,11 @@
-<h1>You are asigned to review a post</h1>
+<h1>You are assigned to review a post</h1>
 
 <p>
     Hello {{ $data['reviewer']->name }},
 </p>
 <p>
-   You are assigned as reviewer for the " {{ $data['post']['title'] }}" article.<br>
-    Please review it as soon as possible, the article is placed in your feed.
-</p>
-<p>
-    Please <a href="{{ route('login') }}" title="">sign in</a> to review the article.
+    You are assigned as reviewer for the article "{{ $data['post']['title'] }}".<br>
+    Please review it as soon as possible. The article was placed in your feed.
 </p>
 <p>
     Blogify
